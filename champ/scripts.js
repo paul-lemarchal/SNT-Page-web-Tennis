7cc85d67130fadb64a912591c233163989364adb
paul-lemarchal <paul.lemarchal@outlook.com>
2024-05-28 22:56:22 +0200

    Add files via upload

---
+++ champ/scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav ul li a');
+
+    for (const link of links) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            
+            window.scrollTo({
+                top: targetElement.offsetTop - document.querySelector('header').offsetHeight,
+                behavior: 'smooth'
+            });
+        });
+    }
+});
